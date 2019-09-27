85b0b2b79b7e0a48ee4adc9fb85fb689e8309e95
Henry Desroches <henry.desroches@promontech.com>
2019-09-27 10:06:09 -0600

    🚧  - .nvmrc and destructuring company into list item

--- components/List.vue
+++ components/List.vue
@@ -2,7 +2,7 @@
   <section class="companies-list" id="the-list">
     <div class="companies-list__header">the list</div>
     <Search />
-    <ListItem v-for="company in companies" :company="company" :key="company.slug" />
+    <ListItem v-for="company in companies" v-bind="company" :key="company.slug" />
   </section>
 </template>
 
@@ -11,6 +11,11 @@ import Search from './Search'
 import ListItem from './ListItem'
 
 export default {
+  data() {
+    return {
+      activeId: null
+    }
+  },
   props: ['companies'],
   components: {
     Search,
--- components/ListItem.vue
+++ components/ListItem.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="company">
     <!-- {{company}} -->
-    <p class="company__title">{{company.title}}</p>
-    <div class="company__description" v-html="$md.render(company.body)" />
-    <a class="company__link" :href="company.link">read more</a>
+    <p class="company__title">{{title}}</p>
+    <div class="company__description" v-html="$md.render(body)" />
+    <a class="company__link" :href="link">read more</a>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['company']
+  props: {
+    title: String,
+    body: String,
+    link: String
+  }
 }
 </script>
